87ab44c1e81e4cdad2a05570d67d464935685f7b
123 <zlata18_00@mail.ru>
2023-08-27 16:07:12 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        task1();}
+        task1();
+    task2();}
     public static void task1(){
         System.out.println("Задача 1");
         for (int i=1;i<11;i++){
             System.out.println(i+" ");
         }
     }
-}
+    public static void task2(){
+        System.out.println("Задача 2");{
+            for (int i=10;i>0;i--){
+                System.out.println(i+" ");
+        }
+    }
+}}
