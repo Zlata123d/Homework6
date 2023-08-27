c54a370d2ff050ecac91c526fa0e3c9c26dab438
123 <zlata18_00@mail.ru>
2023-08-27 16:12:03 +0300

    Задача 4

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         task1();
     task2();
-    task3();}
+    task3();
+    task4();}
     public static void task1(){
         System.out.println("Задача 1");
         for (int i=1;i<11;i++){
@@ -20,4 +21,10 @@ public class Main {
         System.out.println("Задача 3");{
             for (int i=0;i<17;i+=2){
                 System.out.println(i+" ");
-            }}}}
+            }
+        }}
+    public static void task4(){
+        System.out.println("Задача 4");{
+            for (int i=10;i>=-10;i--){
+                System.out.println(i+" ");
+}}}}
