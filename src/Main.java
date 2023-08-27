587fd1f259e55bb1816dfd8774cd4a242da4c1d1
123 <zlata18_00@mail.ru>
2023-08-27 16:20:10 +0300

    Задача 6

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ public class Main {
     task2();
     task3();
     task4();
-    task5();}
+    task5();
+    task6();}
     public static void task1(){
         System.out.println("Задача 1");
         for (int i=1;i<11;i++){
@@ -33,4 +34,9 @@ public class Main {
         System.out.println("Задача 5");{
             for (int i=1904;i<=2096;i+=4) {
                 System.out.println(i + " ");
-            }}}}
+            }}}
+    public static void task6(){
+        System.out.println("Задача 6");{
+            for (int i=7;i<=98;i+=7) {
+                System.out.println(i + " ");
+}}}}
